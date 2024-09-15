b5a4f50df3716fe2ff06e6b732bdef4071a6e4d2
Krzysztof Wozniczak <Krzysztof@DESKTOP-ROJJHH1>
2024-09-15 10:39:33 +0200

    3.2 CSS Modules

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,6 +1,7 @@
+import styles from './Button.module.css';
 
 function Button() {
-  return <button className="button">Click me</button>;
+  return <button className={styles.button}>Click me</button>;
 }   
 
 export default Button;
